b3555e03192032c58279a75e912a733f1ece16a3
jeydere <jeydere@hotmail.com>
2016-08-22 15:16:00 -0400

    update to conflict merge

--- RemoteExample/src/Example.java
+++ RemoteExample/src/Example.java
@@ -2,7 +2,11 @@
 public class Example {
 
 	public static void main(String[] args) {
+		long j =8;
+		String str = "changed";
 		System.out.println("Hello World2");
+		System.out.println("name " + str);
+		System.out.println("number: "+ j);
 
 	}
 
