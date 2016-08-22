d92ac8e53f4c31d91a15e4caacc6e1746d2c067d
Jeylani Dere <jey@somecompany.com>
2016-08-22 15:44:33 -0400

    changed data type of number in branch

--- RemoteExample/src/Example.java
+++ RemoteExample/src/Example.java
@@ -4,11 +4,11 @@ public class Example {
 	public static void main(String[] args) {
 
 		
-		int i =5;
+		long j =5;
 		String name = "test";
-		System.out.println("Hello World2");
+		System.out.println("Hello World3");
 		System.out.println("name " + name);
-		System.out.println("number: "+ i);
+		System.out.println("number: "+ j);
 		
 
 	}
