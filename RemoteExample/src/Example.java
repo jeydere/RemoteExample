353dabf53aa58770b4baf8a4ff1f1e914968a1dd
Jeylani Dere <jey@somecompany.com>
2016-08-22 15:16:52 -0400

    added number and name

--- RemoteExample/src/Example.java
+++ RemoteExample/src/Example.java
@@ -2,7 +2,12 @@
 public class Example {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World2");
+		
+		int i =5;
+		String name = "test";
+		System.out.println("Hello World3");
+		System.out.println("name " + name);
+		System.out.println("number: "+ i);
 
 	}
 
