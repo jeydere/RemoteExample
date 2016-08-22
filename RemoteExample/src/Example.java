0e2bd25e261ca1d324c64ad17d2e27f5626a7460
jeydere <jeydere@hotmail.com>
2016-08-22 16:13:28 -0400

    display method arg

--- RemoteExample/src/Example.java
+++ RemoteExample/src/Example.java
@@ -5,15 +5,20 @@ public class Example {
 
 		
 
-		long j=88;
+		String num = "88";
 		String str = "test";
 		System.out.println("Hello World4");
 		System.out.println("name " + str);
 
-		System.out.println("num: "+ j);
+		System.out.println("num: "+ num);
+		
+		display(num);
 
 		
-
+	}
+	
+	public static void display(String str){
+		System.out.println("display number is :" + str);
 	}
 
 }
