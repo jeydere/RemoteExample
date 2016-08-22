a205cc477a16dbca3db02e8fa396e268c22dcf98
Jeylani Dere <jey@somecompany.com>
2016-08-22 15:46:23 -0400

    changed to Long

--- RemoteExample/src/Example.java
+++ RemoteExample/src/Example.java
@@ -4,11 +4,11 @@ public class Example {
 	public static void main(String[] args) {
 
 		
-		int i =5;
+		Long b =5L;
 		String name = "test";
 		System.out.println("Hello World2");
 		System.out.println("name " + name);
-		System.out.println("number: "+ i);
+		System.out.println("number: "+ b);
 		
 
 	}
